b7da38f35f41086286a76361e84e14d6c4c718ab
aceade@github.com <philiprowlands90@gmail.com>
2023-12-07 15:56:27 +0000

    #1 fixing stlying and links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,28 +11,28 @@ export class HomeComponent {
   constructor() {}
 
   gallery: ImageDetails[] = [{
-    source: "../images/borealis-480w.webp",
+    source: "/sydfjords-angular/images/borealis-480w.webp",
     caption: "The aurora australis over the fjords. Nulla pharetra diam sit amet nisl suscipit adipiscing. Augue lacus viverra vitae congue eu consequat ac felis.",
     altText: "An aurora australis.",
-    srcset: "../images/borealis-480w.webp 480w, ../images/borealis-800w.webp 800w, ../images/borealis-1080w.webp 1080w",
+    srcset: "/sydfjords-angular/images/borealis-480w.webp 480w, /sydfjords-angular/images/borealis-800w.webp 800w, /sydfjords-angular/images/borealis-1080w.webp 1080w",
     sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px"
   },{
-    source: "../images/town2-480w.webp",
+    source: "/sydfjords-angular/images/town2-480w.webp",
     altText: "A collection of buildings near the water's edge, in front of a mountain",
     caption: "Coldvatn, the heart of the Sydfjords. Sed adipiscing diam donec adipiscing tristique risus nec. Risus feugiat in ante metus dictum at.",
-    srcset: "../images/town2-480w.webp 480w, ../images/town2-800w.webp 800w, ../images/town2-1080w.webp 1080w",
+    srcset: "/sydfjords-angular/images/town2-480w.webp 480w, /sydfjords-angular/images/town2-800w.webp 800w, /sydfjords-angular/images/town2-1080w.webp 1080w",
     sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px"
   },{
-    source: "../images/town-480w.webp",
+    source: "/sydfjords-angular/images/town-480w.webp",
     altText: "A town near the water's edge, with snow-capped mountains in the background",
     caption: "Coldwatvn, the heart of the Sydfjords. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Scelerisque purus semper eget duis at",
-    srcset: "../images/town-480w.webp 480w, ../images/town-800w.webp 800w, ../images/town-1080w.webp 1080w",
+    srcset: "/sydfjords-angular/images/town-480w.webp 480w, /sydfjords-angular/images/town-800w.webp 800w, /sydfjords-angular/images/town-1080w.webp 1080w",
     sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px"
   },{
-    source: "../images/horses-480w.webp",
+    source: "/sydfjords-angular/images/horses-480w.webp",
     altText: "A pair of horses",
     caption: "Tortor at auctor urna nunc id cursus metus. A pellentesque sit amet porttitor eget dolor. Tempor orci eu lobortis elementum nibh tellus.",
-    srcset: "../images/horses-480w.webp 480w, ../images/horses-800w.webp 800w, ../images/horses-1080w.webp 1080w",
+    srcset: "/sydfjords-angular/images/horses-480w.webp 480w, /sydfjords-angular/images/horses-800w.webp 800w, /sydfjords-angular/images/horses-1080w.webp 1080w",
     sizes: "(max-width: 600px) 480px, (max-width: 1000px) 800px, 1080px"
   }, ];
 }

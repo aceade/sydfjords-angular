e4710a2050f4a3c0381e45a9c00696ff3e1625ae
aceade@github.com <philiprowlands90@gmail.com>
2023-12-08 09:30:52 +0000

    #2 adding loremvik page

---
+++ src/app/loremvik/loremvik.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loremvik',
+  templateUrl: './loremvik.component.html',
+  styleUrls: ['./loremvik.component.css']
+})
+export class LoremvikComponent {
+
+}

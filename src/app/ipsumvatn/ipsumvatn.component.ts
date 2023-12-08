9d1528d88a48e9bc50e60d65c63f9f250bf9fbb5
aceade@github.com <philiprowlands90@gmail.com>
2023-12-08 09:37:43 +0000

    #2 adding Ipsumvatn and whale-watching page

---
+++ src/app/ipsumvatn/ipsumvatn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ipsumvatn',
+  templateUrl: './ipsumvatn.component.html',
+  styleUrls: ['./ipsumvatn.component.css']
+})
+export class IpsumvatnComponent {
+
+}
